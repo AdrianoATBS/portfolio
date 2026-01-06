af52cbc1d5ede56806ec115e6feac9b06659d152
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-01-06 17:09:38 -0300

    feat: Cria estrutura inicial do portfólio

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter, Poppins } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--fonte-texto",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600","700"],
+  variable: "--fonte-titulos",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${inter.variable} ${poppins.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
