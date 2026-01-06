60c0e2d36d7a530627733568f2780db393c0e3a3
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-01-06 19:41:55 -0300

    feat(ListaHabilidade): adiciona carrosel de habilidades com Swiper

---
+++ src/components/ListaHabilidade.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+
+import { FaReact } from "react-icons/fa";
+import { SiDotnet, SiTypescript, SiVite, SiNextdotjs } from "react-icons/si";
+import { TbBrandCSharp, TbSql } from "react-icons/tb";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaGithub } from "react-icons/fa";
+import { FaDocker } from "react-icons/fa";
+
+export default function ListaHabilidade() {
+  return (
+    <section className="flex flex-col items-center mt-15 ">
+      <h2 className="text-3xl font-[--font-titulos] text-texto mb-6">
+        Minhas Habilidades 
+      </h2>
+
+      <Swiper
+      className="w-full"
+        modules={[Autoplay]}
+        spaceBetween={10}
+        slidesPerView={4}
+        loop={true}
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
+        breakpoints={{
+          320: { slidesPerView: 2 },
+          640: { slidesPerView: 3 },
+          1024: { slidesPerView: 4 },
+        }}
+      >
+        <SwiperSlide><FaReact size={60} className="text-destaque" /></SwiperSlide>
+        <SwiperSlide><SiDotnet size={60} className="text-destaque" /></SwiperSlide>
+        <SwiperSlide><TbBrandCSharp size={60} className="text-destaque" /></SwiperSlide>
+        <SwiperSlide><TbSql size={60} className="text-destaque" /></SwiperSlide>
+        <SwiperSlide><IoLogoJavascript size={60} className="text-destaque" /></SwiperSlide>
+        <SwiperSlide><SiTypescript size={60} className="text-destaque" /></SwiperSlide>
+        <SwiperSlide><SiVite size={60} className="text-destaque" /></SwiperSlide>
+        <SwiperSlide><SiNextdotjs size={60} className="text-destaque" /></SwiperSlide>
+        <SwiperSlide><FaGithub size={60} className="text-destaque" /></SwiperSlide>
+        <SwiperSlide><FaDocker size={60} className="text-destaque" /></SwiperSlide>
+      </Swiper>
+
+    </section>
+  );
+}
