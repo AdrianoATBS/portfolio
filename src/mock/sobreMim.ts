972626b58c58ea44002bd8c51bf83165aaec068b
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-01-13 19:20:39 -0300

    docs(SobreMim): corrige formatação e pontuação de SobreMim

--- src/mock/sobreMim.ts
+++ src/mock/sobreMim.ts
@@ -4,7 +4,7 @@ export interface SobreMim {
 }
 
 export const sobreMim: SobreMim = {
-  pergunta: "Quem sou eu ? ",
+  pergunta: "Quem sou eu? ",
   texto: `Olá, eu sou o Adriano Teixeira Benevenuto Sampaio, sou Desenvolvedor de Software com foco em back-end e atuação crescente em front-end, movido pela curiosidade e pela vontade de criar soluções úteis para pessoas. Minha curiosidade na tecnologia começou aos 15 anos, influenciada por amigos que já atuavam na área, mas foi aos 20 anos, no início de 2024, que decidi mergulhar de vez nesse universo.
 
 Desde então, tenho me dedicado ao estudo constante e ao desenvolvimento de habilidades técnicas e comportamentais. Atualmente curso Análise e Desenvolvimento de Sistemas na Estácio e concluí o programa Estartando Devs, onde tive contato com princípios e práticas avançadas como DDD, CQRS, Clean Code e arquitetura em camadas, além de treinamentos em comunicação e trabalho em equipe.
@@ -17,5 +17,5 @@ Hoje, minhas principais tecnologias incluem C#, .NET, Docker, SQL, Clean Archite
 
 Meus próximos objetivos são conquistar meu primeiro emprego na área, concluir minha graduação e futuramente seguir para uma pós-graduação. Também estou investindo no aprendizado de inglês e tenho grande interesse em cibersegurança, tema que planejo explorar com mais profundidade.
 
-Fora da tecnologia, gosto de jogar e ler — especialmente fantasia — hobbies que alimentam minha criatividade e me ajudam a manter o equilíbrio. Acredito no poder de ambientes de trabalho saudáveis, onde pessoas se apoiam, aprendem juntas e transformam desafios em oportunidades.`
+Fora da tecnologia, gosto de jogar e ler  especialmente fantasia hobbies que alimentam minha criatividade e me ajudam a manter o equilíbrio. Acredito no poder de ambientes de trabalho saudáveis, onde pessoas se apoiam, aprendem juntas e transformam desafios em oportunidades.`
 };
