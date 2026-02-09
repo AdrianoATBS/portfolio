2f662175efd4953c6196478b3e4a449671c15e35
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-02-09 18:59:06 -0300

    style: melhora acessibilidade usabilidade da seção de contato

--- src/components/Contato.tsx
+++ src/components/Contato.tsx
@@ -1,21 +1,31 @@
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+
 import Link from "next/link";
 export default function Contato() {
     return(
         <section className="mt-14">
-        <div className="py-8 px-4 flex flex-col items-center">
-            <h2 className="text-3xl font-[--font-titulos] text-texto mb-4">Contato</h2>
-            <div className="flex gap-6 mb-4">
+        <div className="py-8 flex flex-col items-center">
+            <h2 className="text-3xl font-[--font-titulos] text-texto mb-6">Contato</h2>
+
+            <div className="flex gap-8 ">
                 <Link href="https://www.linkedin.com/in/adriano-teixeira-benevenuto-sampaio-236151330/" 
                 target="_blank" rel="noopener noreferrer" 
+                aria-label="LinkedIn"
                 className="text-texto hover:text-destaque transition-colors">
-                    <FaLinkedin size={50} />
+                    <FaLinkedin size={50}/>
                 </Link>
                 <Link href="https://github.com/AdrianoATBS"
-                 target="_blank" rel="noopener noreferrer"
-                  className="text-texto hover:text-destaque transition-colors">
+                target="_blank" rel="noopener noreferrer"
+                aria-label="GitHub"
+                className="text-texto hover:text-destaque transition-colors">
                     <FaGithub size={50} />
+                </Link>
+                <Link href={"mailto:tiadrianoteixeira1223@gmail.com"}
+                aria-label="Enviar email"
+                 className="text-texto hover:text-destaque transition-colors">
+                    <MdOutlineEmail size={50}  />
                 </Link>
             </div>
         </div>
--- src/components/PequenoSobre.tsx
+++ src/components/PequenoSobre.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function PequenoSobre() {
     return(
-         <section className="pt-16">
+         <section className="pt-14">
             <div className="mx-auto flex max-w-4xl flex-col items-center gap-12 md:flex-row md:translate-x-16">
                 <div className="flex-shrink-0">
                 <Image src="/foto.jpeg" alt="Foto de Perfil" width={200} height={200} className="rounded-full mx-auto mt-8"/>
@@ -10,7 +10,7 @@ export default function PequenoSobre() {
                 <div className="flex flex-col max-w-xl text-left ">
                         <h1 className="text-4xl font-bold mb-4 font-[--fonte-titulos] text-texto">
                         Olá, eu sou Adriano</h1>
-                    <p className=" text-base  font-[--fonte-texto]  text-texto-secundario leading-relaxed">
+                    <p className=" text-lg  font-[--fonte-texto]  text-texto-secundario leading-relaxed">
                         Sou desenvolvedor de software com foco em back-end e front-end.
                         No back-end, trabalho com C#, .NET, Clean Architecture, SQL e Docker.
                         No front-end, desenvolvo interfaces modernas utilizando JavaScript, TypeScript, React, Next.js e Tailwind CSS.
